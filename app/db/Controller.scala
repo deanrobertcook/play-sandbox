b2c35528bfd598409da8cdf632a03ce4fd2620b9
Dean Cook <deanrobertcook@gmail.com>
2021-07-28 13:36:35 +0200

    Get working connection to database with Anorm

--- app/db/Controller.scala
+++ app/db/Controller.scala
@@ -1,13 +1,23 @@
 package db
 
+import anorm.{Macro, RowParser, SqlStringInterpolation}
 import com.google.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
-import anorm._
+import play.api.db.Database
 
-class Controller @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
+case class Employee(firstName: String,
+                    lastName: String)
 
-  def database() = Action {
-    Ok(s"Test database call")
+class Controller @Inject() (db: Database, cc: ControllerComponents) extends AbstractController(cc) {
+
+  val parser: RowParser[Employee] = Macro.parser[Employee]("first_name", "last_name")
+
+  def dbCall() = Action {
+    val res = db.withConnection { implicit c =>
+      SQL"SELECT * FROM employees".as(parser.*)
+    }
+
+    Ok(s"Test database call $res")
   }
 
 }
