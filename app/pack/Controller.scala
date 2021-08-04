ee894a16a46924c802735cb787a68836be6e67aa
Dean Cook <deanrobertcook@gmail.com>
2021-08-04 16:21:02 +0200

    Playing with anorm more

--- app/pack/Controller.scala
+++ app/pack/Controller.scala
@@ -4,6 +4,7 @@ import anorm.{Macro, RowParser, SqlStringInterpolation}
 import macrotest.IdentityMacro
 import play.api.db.Database
 import play.api.mvc.{AbstractController, ControllerComponents}
+import anorm.SqlParser._
 
 import javax.inject.Inject
 
@@ -24,6 +25,27 @@ class Controller @Inject() (db: Database, cc: ControllerComponents, tl: TestClas
     Ok(s"Test database call $res")
   }
 
+  def employee(id: Int) = Action {
+    val res = db.withConnection { implicit c =>
+      SQL""" 
+        SELECT first_name, last_name, dept_no 
+        FROM employees e
+        JOIN dept_emp d ON d.emp_no = e.emp_no 
+        WHERE e.emp_no = $id
+      """.as(parser.*)
+    }.head
+
+    Ok(s"Employee with id: $id: $res")
+  }
+
+  def employeeCount() = Action {
+    val count: Long = db.withConnection { implicit c => 
+      SQL"SELECT count(*) FROM employees".as(scalar[Long].single)
+    }
+
+    Ok(s"Number of employees: $count")
+  }
+  
   def departments() = Action {
     val res = db.withConnection { implicit c =>
       SQL"""
@@ -37,7 +59,6 @@ class Controller @Inject() (db: Database, cc: ControllerComponents, tl: TestClas
   }
 
   def testMacro() = Action {
-//    val testClass = TestClass("INput")
     Ok(s"TestClass output ${tl.simpleMethod()}")
   }
 
